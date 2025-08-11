12db84caacd4f82cc364d464c7c9f8552400a6ae
Renato Almeida <renato.m.alm@gmail.com>
2025-08-12 00:42:34 +0100

    testing

--- kmvi/build.gradle.kts
+++ kmvi/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 group = "io.github.natobytes"
 //version = System.getenv()["RELEASE_NAME"] ?: "0.1.0"
-version = "0.1.0-SNAPSHOT"
+version = "0.1.1"
 
 
 kotlin {
@@ -72,7 +72,7 @@ publishing {
 }
 
 mavenPublishing {
-    publishToMavenCentral()
+    publishToMavenCentral(false)
 
     signAllPublications()
 
